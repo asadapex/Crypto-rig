82252b83a9b2330dfcd7fea30347fdbe2e61993a
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-06-30 23:24:56 +0500

    cors fixed

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'https://dev-api.cryptorig.uz',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
