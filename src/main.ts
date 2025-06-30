2262b268c8f717e9d47dd8e55e602f9a51cf0d4d
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-06-30 23:33:17 +0500

    cors fixed v2

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://dev-api.cryptorig.uz',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
