cf1e2ef50bc18a8609ac6d83db1bf2f62af494d1
asadapex <mrasad.apex@gmail.com>
2025-07-13 13:07:38 +0500

    vdcard rbac

--- src/videocard/videocard.controller.ts
+++ src/videocard/videocard.controller.ts
@@ -6,15 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { VideocardService } from './videocard.service';
 import { CreateVideocardDto } from './dto/create-videocard.dto';
 import { UpdateVideocardDto } from './dto/update-videocard.dto';
+import { AuthGuard } from 'src/jwtauth/jwtauth.guard';
+import { RolesGuard } from 'src/role/role.guard';
+import { Roles } from 'src/decorators/role-decorator';
+import { UserRole } from '@prisma/client';
 
 @Controller('videocard')
 export class VideocardController {
   constructor(private readonly videocardService: VideocardService) {}
 
+  @Roles(UserRole.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createVideocardDto: CreateVideocardDto) {
     return this.videocardService.create(createVideocardDto);
@@ -24,12 +32,14 @@ export class VideocardController {
   findAll() {
     return this.videocardService.findAll();
   }
-
-  @Get(':id')
+  @Get()
   findOne(@Param('id') id: string) {
     return this.videocardService.findOne(id);
   }
 
+  @Roles(UserRole.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +48,9 @@ export class VideocardController {
     return this.videocardService.update(id, updateVideocardDto);
   }
 
+  @Roles(UserRole.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.videocardService.remove(id);
