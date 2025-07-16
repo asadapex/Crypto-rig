0e762e935b06ed5ed5262c0010e2bef2e01d1f73
asadapex <mrasad.apex@gmail.com>
2025-07-16 18:12:13 +0500

    log token

--- src/jwtauth/jwtauth.guard.ts
+++ src/jwtauth/jwtauth.guard.ts
@@ -27,7 +27,8 @@ export class AuthGuard implements CanActivate {
     }
 
     const token = authHeader.split(' ')[1];
-
+    console.log(token);
+    
     try {
       const data = this.jwt.verify(token);
       req['user-id'] = data.id;
