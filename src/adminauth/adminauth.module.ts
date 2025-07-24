d7f5112affec9869830fdf4a3629616b6bc28333
asadapex <mrasad.apex@gmail.com>
2025-07-24 23:12:16 +0500

    admin module fixed

--- src/adminauth/adminauth.module.ts
+++ src/adminauth/adminauth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminauthService } from './adminauth.service';
 import { AdminauthController } from './adminauth.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [AdminauthController],
   providers: [AdminauthService],
 })
