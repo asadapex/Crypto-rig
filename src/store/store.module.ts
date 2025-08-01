83cac3287930c955040568a643ad18f918250575
asadapex <mrasad.apex@gmail.com>
2025-08-01 09:48:52 +0500

    error fixed

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { StoreController } from './store.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [StoreController],
   providers: [StoreService],
 })
